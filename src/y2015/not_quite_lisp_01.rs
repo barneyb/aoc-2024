497151573e93216621c243b72ce674109b4672c0
Barney Boisvert <bboisvert@gmail.com>
2024-10-27 11:00:02 -0700

    do Not Quite Lisp in a single pass as well

--- src/y2015/not_quite_lisp_01.rs
+++ src/y2015/not_quite_lisp_01.rs
@@ -21,6 +21,22 @@ pub fn part_two(input: &str) -> usize {
     panic!("never entered basement?!")
 }
 
+pub fn both_parts(input: &str) -> (i32, usize) {
+    let mut f = 0;
+    let mut entered_basement = None;
+    for (i, c) in input.chars().enumerate() {
+        f = match c {
+            '(' => f + 1,
+            ')' => f - 1,
+            _ => f,
+        };
+        if f == -1 && entered_basement.is_none() {
+            entered_basement = Some(i + 1);
+        }
+    }
+    (f, entered_basement.expect("Should have entered basement"))
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -42,6 +58,17 @@ mod test {
         with_input(2015, 1, |input, tx| {
             tx.send(Part::A(Box::new(part_one(input)))).unwrap();
             tx.send(Part::B(Box::new(part_two(input)))).unwrap();
+        })
+        .unwrap();
+    }
+
+    #[test]
+    fn test_real_input_as_one() {
+        use crate::{with_input, Part};
+        with_input(2015, 1, |input, tx| {
+            let (a, b) = both_parts(input);
+            tx.send(Part::A(Box::new(a))).unwrap();
+            tx.send(Part::B(Box::new(b))).unwrap();
         })
         .unwrap();
     }

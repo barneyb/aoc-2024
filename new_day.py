d393eff6a306143b29ea1c2112b893f2329bae50
Barney Boisvert <bboisvert@gmail.com>
2024-11-02 09:18:32 -0700

    gen a binary on new days too

--- new_day.py
+++ new_day.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import datetime
 import re
 import subprocess
@@ -12,12 +13,15 @@ year = int(sys.argv[1]) if len(sys.argv) >= 2 else aoc_now.year
 puzzle = Puzzle(year=year, day=day)
 print(f"\nInput for {year} Day {day}:\n---------------------\n{puzzle.input_data}\n")
 
+yyear = f"y{year}"
+zday = str(day) if day >= 10 else f"0{day}"
+name = re.sub("[^a-z0-9]+", "_", puzzle.title.lower())
 params = dict(
     year = year,
-    yyear = f"y{year}",
+    yyear = yyear,
     day = day,
-    zday = str(day) if day >= 10 else f"0{day}",
-    name = re.sub("[^a-z0-9]+", "_", puzzle.title.lower()),
+    zday = zday,
+    name = name,
 )
 
 print(params)
@@ -28,15 +32,17 @@ if subprocess.run(["git", "diff", "--exit-code"]).returncode != 0:
 subprocess.run(["git", "fetch"], check=True)
 # if master is at/after origin/master, use master, otherwise use origin/master
 start_ref ="master" if subprocess.run(["git", "merge-base", "--is-ancestor", "origin/master", "master"]).returncode == 0 else "origin/master"
-subprocess.run(["git", "checkout", "-b", f"{year}/{day}", "--no-track", start_ref], check=True)
+subprocess.run(["git", "checkout", "-b", f"{year}/{zday}", "--no-track", start_ref], check=True)
 
-year_filename = f"./src/{params['yyear']}.rs"
+year_filename = f"./src/{yyear}.rs"
+module_filename = f"./src/{yyear}/{name}_{zday}.rs"
+binary_filename = f"./src/bin/{name}.rs"
+
 with open(year_filename, "a", encoding="utf-8") as f:
-    f.write(f"mod {params['name']}_{params['zday']};\n")
+    f.write(f"pub mod {name}_{zday};\n")
 
-subprocess.run(["mkdir", "-p", f"./src/{params['yyear']}"], check=True)
-module_file_name = f"./src/{params['yyear']}/{params['name']}_{params['zday']}.rs"
-with open(module_file_name, "w", encoding="utf-8") as f:
+subprocess.run(["mkdir", "-p", f"./src/{yyear}"], check=True)
+with open(module_filename, "w", encoding="utf-8") as f:
     f.write(Template("""pub fn part_one(input: &str) -> usize {
     input.len()
 }
@@ -71,5 +77,21 @@ mod test {
 }
 """).substitute(params))
 
-subprocess.run(["git", "add", year_filename, module_file_name], check=True)
-subprocess.run(["idea", module_file_name])
+with open(binary_filename, "w", encoding="utf-8") as f:
+    f.write(Template("""use aoc::$yyear::${name}_$zday::part_one;
+use aoc::{with_input, Part};
+use std::io::Error;
+
+fn main() -> Result<(), Error> {
+    with_input($year, $day, |input, tx| {
+        tx.send(Part::Other(Box::new(part_one(input)))).unwrap();
+        // tx.send(Part::A(Box::new(part_one(input)))).unwrap();
+        // tx.send(Part::B(Box::new(part_two(input)))).unwrap();
+    })
+}
+""").substitute(params))
+
+subprocess.run(["cargo", "run", "--bin", name], check=True)
+subprocess.run(["git", "add", year_filename, module_filename, binary_filename], check=True)
+subprocess.run(["idea", binary_filename])
+subprocess.run(["idea", module_filename])

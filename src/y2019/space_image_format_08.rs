f7ee2ed8ba48e50526228ff294fd607106b3ed24
Barney Boisvert <bboisvert@gmail.com>
2024-11-03 20:00:32 -0800

    part one

--- src/y2019/space_image_format_08.rs
+++ src/y2019/space_image_format_08.rs
@@ -1,44 +1,45 @@
 use crate::Part;
 use std::sync::mpsc::Sender;
 
+const WIDTH: usize = 25;
+const HEIGHT: usize = 6;
+
 pub fn do_solve(input: &str, tx: Sender<Part>) {
-    tx.send(Part::Other(part_one(input).to_string())).unwrap();
+    tx.send(Part::A(part_one(input).to_string())).unwrap();
 }
 
 fn part_one(input: &str) -> usize {
-    input.len()
+    // blindly assume single-byte characters
+    if let Some(((_, o, t), _)) = input
+        .as_bytes()
+        .chunks(WIDTH * HEIGHT)
+        .map(|bs| String::from_utf8(bs.into()).unwrap())
+        .map(|s| {
+            let zc = s.chars().fold((0, 0, 0), |(z, o, t), c| match c {
+                '0' => (z + 1, o, t),
+                '1' => (z, o + 1, t),
+                '2' => (z, o, t + 1),
+                _ => (z, o, t),
+            });
+            (zc, s)
+        })
+        .min()
+    {
+        return o * t;
+    }
+    panic!("no layers?!")
 }
 
-fn part_two(input: &str) -> usize {
-    input.len()
-}
+// fn part_two(input: &str) -> usize {
+//     input.len()
+// }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
-    const EXAMPLE_1: &str = r#"123456789012"#;
-
-    const EXAMPLE_2: &str = r#"0222112222120000"#;
-
     #[test]
-    fn example_1() {
-        /*
-         im=3x2
-         */
-        assert_eq!(r"1", part_one(EXAMPLE_1).to_string());
+    fn test_real_input() {
+        crate::with_input(2019, 8, do_solve).unwrap();
     }
-
-    #[test]
-    fn example_2() {
-        /*
-         im=2x2
-         */
-        assert_eq!(r"⟋", part_two(EXAMPLE_2).to_string());
-    }
-
-    // #[test]
-    // fn test_real_input() {
-    //     crate::with_input(2019, 8, do_solve).unwrap();
-    // }
 }

ab9ec49fbd78ea4a73b43bcbfbcd1f082a21c152
Barney Boisvert <bboisvert@gmail.com>
2024-11-17 09:44:53 -0800

    skeleton for 2020 day 11: Seating System

---
+++ src/y2020/seating_system_11.rs
@@ -0,0 +1,42 @@
+use crate::Part;
+use std::sync::mpsc::Sender;
+
+pub fn do_solve(input: &str, tx: Sender<Part>) {
+    tx.send(Part::Other(part_one(input).to_string())).unwrap();
+    // tx.send(Part::Other(part_two(input).to_string())).unwrap();
+}
+
+fn part_one(input: &str) -> usize {
+    input.len()
+}
+
+// fn part_two(input: &str) -> usize {
+//     input.len()
+// }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const EXAMPLE_1: &str = r#"L.LL.LL.LL
+LLLLLLL.LL
+L.L.L..L..
+LLLL.LL.LL
+L.LL.LL.LL
+L.LLLLL.LL
+..L.L.....
+LLLLLLLLLL
+L.LLLLLL.L
+L.LLLLL.LL"#;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(r"37", part_one(EXAMPLE_1).to_string());
+        // assert_eq!(r"26", part_two(EXAMPLE_1).to_string());
+    }
+
+    // #[test]
+    // fn test_real_input() {
+    //     crate::with_input(2020, 11, do_solve).unwrap();
+    // }
+}

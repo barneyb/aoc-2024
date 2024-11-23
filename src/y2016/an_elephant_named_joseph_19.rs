a3f5d884cc9338ccd908a7d606ed3e9a33a88f9b
Barney Boisvert <bboisvert@gmail.com>
2024-11-23 11:33:36 -0800

    part two

--- src/y2016/an_elephant_named_joseph_19.rs
+++ src/y2016/an_elephant_named_joseph_19.rs
@@ -3,41 +3,67 @@ use std::sync::mpsc::Sender;
 
 pub fn do_solve(input: &str, tx: Sender<Part>) {
     tx.send(Part::A(part_one(input).to_string())).unwrap();
-    // tx.send(Part::Other(part_two(input).to_string())).unwrap();
+    tx.send(Part::B(part_two(input).to_string())).unwrap();
 }
 
 fn part_one(input: &str) -> usize {
-    let mut size: usize = input.parse().unwrap();
-    let mut list = Vec::with_capacity(size + 1);
-    list.push(0); // there's no elf 0 - waste a slot
-    for i in 1..size {
-        list.push(i + 1)
-    }
-    list.push(1); // link the ring
+    let mut circle = build_circle(input);
+    let mut count = circle.len() - 1;
     let mut curr = 1;
-    while size > 1 {
-        let to_kill = list[curr];
-        let following = list[to_kill];
-        list[curr] = following;
+    while count > 1 {
+        let to_kill = circle[curr];
+        let following = circle[to_kill];
+        circle[curr] = following;
         curr = following;
-        size -= 1;
+        count -= 1;
     }
     curr
 }
 
-// fn part_two(input: &str) -> usize {
-//     input.len()
-// }
+fn build_circle(input: &str) -> Vec<usize> {
+    let count: usize = input.parse().unwrap();
+    let mut list = Vec::with_capacity(count + 1);
+    list.push(0); // there's no elf 0 - waste a slot
+    for i in 1..count {
+        list.push(i + 1)
+    }
+    list.push(1); // link into ring
+    list
+}
+
+fn part_two(input: &str) -> usize {
+    let mut circle = build_circle(input);
+    let mut count = circle.len() - 1;
+    let mut curr = 1;
+    let mut prev_opp = count / 2;
+    while count > 1 {
+        let to_kill = circle[prev_opp];
+        let following = circle[to_kill];
+        circle[prev_opp] = following;
+        if count % 2 == 1 {
+            prev_opp = following;
+        }
+        curr = circle[curr];
+        count -= 1;
+    }
+    curr
+}
 
 #[cfg(test)]
 mod test {
     use super::*;
 
-    const EXAMPLE_1: &str = r#"5"#;
+    #[test]
+    fn test_part_one() {
+        assert_eq!(3, part_one("5"));
+        assert_eq!(5, part_one("6"));
+    }
 
     #[test]
-    fn example_1() {
-        assert_eq!(r"3", part_one(EXAMPLE_1).to_string());
+    fn test_part_two() {
+        assert_eq!(2, part_two("5"));
+        assert_eq!(3, part_two("6"));
+        assert_eq!(3, part_two("12"));
     }
 
     #[test]

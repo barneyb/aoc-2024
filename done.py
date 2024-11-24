d2e3e22bbe0d8bbd890ebf170bf0dbba7098b510
Barney Boisvert <bboisvert@gmail.com>
2024-11-24 13:52:22 -0800

    auto-commit dependency changes

--- done.py
+++ done.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import subprocess
 
+from lib import current_branch
 from status import print_status
 
 if subprocess.run(["git", "diff", "--exit-code"]).returncode != 0:
@@ -9,13 +10,11 @@ if subprocess.run(["git", "diff", "--exit-code"]).returncode != 0:
     print()
     exit(1)
 
+branch = current_branch()
+if branch == "master":
+    print("You're already on master?!")
+    exit(2)
 subprocess.run(["cargo", "test", "--profile", "release"], check=True)
-branch = subprocess.run(
-    ["git", "name-rev", "--name-only", "HEAD"],
-    capture_output=True,
-    text=True,
-    check=True,
-).stdout.strip()
 subprocess.run(["git", "checkout", "master"], check=True)
 subprocess.run(["git", "pull"], check=True)
 subprocess.run(["git", "merge", "--no-edit", branch], check=True)
--- lib.py
+++ lib.py
@@ -1,6 +1,8 @@
 import datetime
 import json
 import os
+import subprocess
+from doctest import master
 from zoneinfo import ZoneInfo
 
 YD = (int, int)
@@ -17,18 +19,50 @@ END = "\033[0m"
 DEPS_FILE = ".deps.json"
 
 
+def current_branch():
+    return subprocess.run(
+        ["git", "name-rev", "--name-only", "HEAD"],
+        capture_output=True,
+        text=True,
+        check=True,
+    ).stdout.strip()
+
+
 def load_deps() -> Deps:
     if not os.path.isfile(DEPS_FILE):
         return dict()
-    with open(DEPS_FILE, "r", encoding="utf-8") as f:
-        return dict(
-            [(tuple(d), {tuple(d) for d in ds}) for [d, ds] in json.load(f) if ds]
-        )
+    s = subprocess.run(
+        ["git", "cat-file", "--textconv", f"master:{DEPS_FILE}"],
+        capture_output=True,
+        text=True,
+        check=True,
+    ).stdout.strip()
+    return dict([(tuple(d), {tuple(d) for d in ds}) for [d, ds] in json.loads(s) if ds])
 
 
 def save_deps(deps: Deps):
+    wc_dirty = subprocess.run(["git", "diff", "--exit-code"]).returncode != 0
+    if wc_dirty:
+        subprocess.run(["git", "stash"], check=True)
+    branch = current_branch()
+    if branch != master:
+        subprocess.run(["git", "checkout", "master"])
     with open(DEPS_FILE, "w", encoding="utf-8") as f:
-        json.dump([(d, sorted(list(ds))) for d, ds in sorted(deps.items()) if ds], f)
+        # Unclear why 'f' (a text-mode file) isn't considered 'SupportsWrite[str]'
+        # noinspection PyTypeChecker
+        json.dump(
+            [(d, sorted(list(ds))) for d, ds in sorted(deps.items()) if ds],
+            f,
+            indent=4,
+            sort_keys=True,
+        )
+    subprocess.run(
+        ["git", "commit", "-m", f"update dependency graph", "--", DEPS_FILE], check=True
+    )
+    if branch != master:
+        subprocess.run(["git", "checkout", branch])
+    if wc_dirty:
+        subprocess.run(["git", "stash", "pop"], check=True)
 
 
 def last_day_of_year(year):

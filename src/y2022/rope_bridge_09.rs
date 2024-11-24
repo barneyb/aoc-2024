d06d48fc522aeccb2ec643837e3737dbee9683fd
Barney Boisvert <bboisvert@gmail.com>
2024-11-24 09:11:39 -0800

    part two

--- src/y2022/rope_bridge_09.rs
+++ src/y2022/rope_bridge_09.rs
@@ -5,22 +5,38 @@ use std::sync::mpsc::Sender;
 
 pub fn do_solve(input: &str, tx: Sender<Part>) {
     tx.send(Part::A(part_one(input).to_string())).unwrap();
-    // tx.send(Part::Other(part_two(input).to_string())).unwrap();
+    tx.send(Part::B(part_two(input).to_string())).unwrap();
 }
 
 type Pt = (i32, i32);
 
 fn part_one(input: &str) -> usize {
+    either_part(input, 1)
+}
+
+fn part_two(input: &str) -> usize {
+    either_part(input, 9)
+}
+
+fn either_part(input: &str, tail_len: usize) -> usize {
     let mut head = (0, 0);
-    let mut tail = (0, 0);
-    let mut visited = HashSet::from([tail]);
+    let mut trailing = vec![head; tail_len];
+    let mut visited = HashSet::from([head]);
     for line in input.lines() {
         let dir: Dir = line.into();
         let n: u32 = line[2..].parse().unwrap();
         for _ in 0..n {
             head = step(head, dir);
-            tail = drag(head, tail);
-            visited.insert(tail);
+            let mut prev = head;
+            trailing = trailing
+                .iter()
+                .map(|&curr| {
+                    let next = drag(prev, curr);
+                    prev = next;
+                    next
+                })
+                .collect();
+            visited.insert(*trailing.iter().last().unwrap());
         }
     }
     visited.len()
@@ -40,22 +56,21 @@ fn step(curr: Pt, dir: Dir) -> Pt {
 #[rustfmt::skip]
 fn drag(head: Pt, tail: Pt) -> Pt {
     let (x, y) = tail;
+    // The bottom four arms have an unreachable +/-2 in their second part. I've
+    // left them for visual symmetry.
+    #[allow(unreachable_patterns)]
     match (x - head.0, y - head.1) {
-        ( 2,  0)            => (x - 1, y    ),
-        (-2,  0)            => (x + 1, y    ),
-        ( 0,  2)            => (x    , y - 1),
-        ( 0, -2)            => (x    , y + 1),
-        ( 1,  2) | ( 2,  1) => (x - 1, y - 1),
-        ( 2, -1) | ( 1, -2) => (x - 1, y + 1),
-        (-2, -1) | (-1, -2) => (x + 1, y + 1),
-        (-1,  2) | (-2,  1) => (x + 1, y - 1),
+        (      2,       0)                     => (x - 1, y    ),
+        (     -2,       0)                     => (x + 1, y    ),
+        (      0,       2)                     => (x    , y - 1),
+        (      0,      -2)                     => (x    , y + 1),
+        ( 1 |  2,       2) | (      2, 1 |  2) => (x - 1, y - 1),
+        (      2, -1 | -2) | ( 1 |  2,     -2) => (x - 1, y + 1),
+        (     -2, -1 | -2) | (-1 | -2,     -2) => (x + 1, y + 1),
+        (-1 | -2,       2) | (     -2, 1 |  2) => (x + 1, y - 1),
         _ => tail,
     }
 }
-
-// fn part_two(input: &str) -> usize {
-//     99999
-// }
 
 #[cfg(test)]
 mod test {
@@ -82,12 +97,28 @@ U 20"#;
     #[test]
     fn example_1() {
         assert_eq!(r"13", part_one(EXAMPLE_1).to_string());
-        // assert_eq!(r"1", part_two(EXAMPLE_1).to_string());
+        assert_eq!(r"1", part_two(EXAMPLE_1).to_string());
     }
 
     #[test]
     fn example_2() {
-        // assert_eq!(r"36", part_two(EXAMPLE_2).to_string());
+        assert_eq!(r"36", part_two(EXAMPLE_2).to_string());
+    }
+
+    #[test]
+    fn test_drag() {
+        // same spot
+        assert_eq!((0, 0), drag((0, 0), (0, 0)));
+        // en passant capture away
+        assert_eq!((0, 0), drag((1, 0), (0, 0)));
+        // pawn's capture away
+        assert_eq!((0, 0), drag((-1, 1), (0, 0)));
+        // rook's capture away
+        assert_eq!((0, 1), drag((0, 2), (0, 0)));
+        // knight's capture away
+        assert_eq!((-1, -1), drag((-1, -2), (0, 0)));
+        // bishop's capture away
+        assert_eq!((1, -1), drag((2, -2), (0, 0)));
     }
 
     #[test]

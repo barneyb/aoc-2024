1a52b3a1adf4d2393148aeb05c6f2852141da238
Barney Boisvert <bboisvert@gmail.com>
2024-11-27 19:17:35 -0800

    part two

--- src/y2021/the_treachery_of_whales_07.rs
+++ src/y2021/the_treachery_of_whales_07.rs
@@ -3,42 +3,46 @@ use std::sync::mpsc::Sender;
 
 pub fn do_solve(input: &str, tx: Sender<Part>) {
     tx.send(Part::A(part_one(input).to_string())).unwrap();
-    // tx.send(Part::Other(part_two(input).to_string())).unwrap();
+    tx.send(Part::B(part_two(input).to_string())).unwrap();
 }
 
 fn part_one(input: &str) -> usize {
-    let mut crabs: Vec<usize> = input
+    either_part(input, |d| d)
+}
+
+fn either_part<F>(input: &str, cost_to_move: F) -> usize
+where
+    F: Fn(usize) -> usize,
+{
+    let mut crabs: Vec<_> = input
         .split(',')
         .map(|c| c.parse::<usize>().unwrap())
         .collect();
     crabs.sort();
+    let fuel_to = |pos: usize| crabs.iter().map(|&c| cost_to_move(pos.abs_diff(c))).sum();
     let mut lo = crabs[0];
     let mut hi = crabs[crabs.len() - 1];
     while lo < hi {
         let mid = (lo + hi) / 2;
-        if is_positive_slope(&crabs, mid) {
+        let a = fuel_to(mid);
+        let b = fuel_to(mid + 1);
+        if a < b {
             hi = mid;
         } else {
             lo = mid + 1;
+            if a == b {
+                // no curvature
+                break;
+            }
         }
     }
     // sorta silly to recompute, but whatever
-    fuel_to(&crabs, lo)
+    fuel_to(lo)
 }
 
-fn is_positive_slope(crabs: &Vec<usize>, pos: usize) -> bool {
-    let a = fuel_to(crabs, pos);
-    let b = fuel_to(crabs, pos + 1);
-    a < b
+fn part_two(input: &str) -> usize {
+    either_part(input, |d| d * (d + 1) / 2)
 }
-
-fn fuel_to(crabs: &Vec<usize>, pos: usize) -> usize {
-    crabs.iter().map(|c| c.abs_diff(pos)).sum()
-}
-
-// fn part_two(input: &str) -> usize {
-//     99999
-// }
 
 #[cfg(test)]
 mod test {
@@ -49,7 +53,7 @@ mod test {
     #[test]
     fn example_1() {
         assert_eq!(r"37", part_one(EXAMPLE_1).to_string());
-        // assert_eq!(r"168", part_two(EXAMPLE_1).to_string());
+        assert_eq!(r"168", part_two(EXAMPLE_1).to_string());
     }
 
     #[test]

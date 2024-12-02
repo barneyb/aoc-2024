f25b4ddeaf7107be055fd544e6f3f8ad31acf642
Barney Boisvert <bboisvert@gmail.com>
2024-12-02 07:35:47 -0800

    part one

--- src/y2024/red_nosed_reports_02.rs
+++ src/y2024/red_nosed_reports_02.rs
@@ -2,12 +2,45 @@ use crate::Part;
 use std::sync::mpsc::Sender;
 
 pub fn do_solve(input: &str, tx: Sender<Part>) {
-    tx.send(Part::Other(part_one(input).to_string())).unwrap();
+    tx.send(Part::A(part_one(input).to_string())).unwrap();
     // tx.send(Part::Other(part_two(input).to_string())).unwrap();
 }
 
-fn part_one(_input: &str) -> usize {
-    99999
+fn part_one(input: &str) -> usize {
+    input
+        .lines()
+        .map(|l| {
+            l.split_ascii_whitespace()
+                .map(|n| n.parse().unwrap())
+                .collect()
+        })
+        .filter(is_safe)
+        .count()
+}
+
+fn is_safe(report: &Vec<usize>) -> bool {
+    if report.len() == 1 {
+        return true;
+    }
+    let mut prev = report[0];
+    if prev < report[1] {
+        // increasing
+        for &curr in &report[1..] {
+            if curr <= prev || curr - prev > 3 {
+                return false;
+            }
+            prev = curr;
+        }
+    } else {
+        // decreasing
+        for &curr in &report[1..] {
+            if curr >= prev || prev - curr > 3 {
+                return false;
+            }
+            prev = curr;
+        }
+    }
+    true
 }
 
 // fn part_two(input: &str) -> usize {
@@ -30,8 +63,8 @@ mod test {
         assert_eq!(r"2", part_one(EXAMPLE_1).to_string());
     }
 
-    // #[test]
-    // fn test_real_input() {
-    //     crate::with_input(2024, 2, do_solve).unwrap();
-    // }
+    #[test]
+    fn test_real_input() {
+        crate::with_input(2024, 2, do_solve).unwrap();
+    }
 }

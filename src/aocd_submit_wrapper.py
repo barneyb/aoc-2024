f2e9207cdaa49d8f501fb226eb05fc0da0f0002d
Barney Boisvert <bboisvert@gmail.com>
2024-12-03 08:13:29 -0800

    don't reopen the browser on submission

--- src/aocd_submit_wrapper.py
+++ src/aocd_submit_wrapper.py
@@ -1,12 +1,15 @@
 import sys
+
 from aocd.models import Puzzle
+from aocd.post import submit
 
 [_, year, day, part, val] = sys.argv
 if part != "a" and part != "b":
     raise TypeError(f"Unknown '{part}' part")
 puzzle = Puzzle(year=int(year), day=int(day))
 if not getattr(puzzle, f"answered_{part}"):
-    setattr(puzzle, f"answer_{part}", val)
+    # this is a little silly, but Puzzle itself doesn't offer a reopen flag
+    submit(val, part=part, day=puzzle.day, year=puzzle.year, reopen=False)
     if getattr(puzzle, f"answered_{part}"):
         exit(0)  # woo!
     else:

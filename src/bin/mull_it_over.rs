da44c6dc20361ab7b0a3fc3be4111c0829f278e4
Barney Boisvert <bboisvert@gmail.com>
2024-12-03 08:15:06 -0800

    skeleton for day 3: Mull It Over

---
+++ src/bin/mull_it_over.rs
@@ -0,0 +1,6 @@
+use aoc::y2024::mull_it_over_03::do_solve;
+use std::io::Error;
+
+fn main() -> Result<(), Error> {
+    aoc::with_input(2024, 3, do_solve)
+}
---
+++ src/y2024/mull_it_over_03.rs
@@ -0,0 +1,33 @@
+use crate::Part;
+use std::sync::mpsc::Sender;
+
+pub fn do_solve(input: &str, tx: Sender<Part>) {
+    tx.send(Part::Other(part_one(input).to_string())).unwrap();
+    // tx.send(Part::Other(part_two(input).to_string())).unwrap();
+}
+
+fn part_one(_input: &str) -> usize {
+    99999
+}
+
+// fn part_two(input: &str) -> usize {
+//     99999
+// }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const EXAMPLE_1: &str =
+        r#"xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"#;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(r"mul", part_one(EXAMPLE_1).to_string());
+    }
+
+    // #[test]
+    // fn test_real_input() {
+    //     crate::with_input(2024, 3, do_solve).unwrap();
+    // }
+}

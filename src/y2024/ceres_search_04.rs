4070e7ebf37d7c40c31b38d4e8a3c1cbcc489251
Barney Boisvert <bboisvert@gmail.com>
2024-12-03 21:01:19 -0800

    skeleton for day 4: Ceres Search

---
+++ src/y2024/ceres_search_04.rs
@@ -0,0 +1,36 @@
+use crate::Part;
+use std::sync::mpsc::Sender;
+
+pub fn do_solve(input: &str, tx: Sender<Part>) {
+    tx.send(Part::Other(part_one(input).to_string())).unwrap();
+    // tx.send(Part::Other(part_two(input).to_string())).unwrap();
+}
+
+fn part_one(_input: &str) -> usize {
+    99999
+}
+
+// fn part_two(input: &str) -> usize {
+//     99999
+// }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const EXAMPLE_1: &str = r#"..X...
+.SAMX.
+.A..A.
+XMAS.S
+.X...."#;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(r"XMAS", part_one(EXAMPLE_1).to_string());
+    }
+
+    // #[test]
+    // fn test_real_input() {
+    //     crate::with_input(2024, 4, do_solve).unwrap();
+    // }
+}

cb20491aca80c6953d815b59a8b8e2a7ece97606
Barney Boisvert <bboisvert@gmail.com>
2024-12-04 21:08:21 -0800

    skeleton for day 5: Print Queue

---
+++ src/bin/print_queue.rs
@@ -0,0 +1,6 @@
+use aoc::y2024::print_queue_05::do_solve;
+use std::io::Error;
+
+fn main() -> Result<(), Error> {
+    aoc::with_input(2024, 5, do_solve)
+}
---
+++ src/y2024/print_queue_05.rs
@@ -0,0 +1,59 @@
+use crate::Part;
+use std::sync::mpsc::Sender;
+
+pub fn do_solve(input: &str, tx: Sender<Part>) {
+    tx.send(Part::Other(part_one(input).to_string())).unwrap();
+    // tx.send(Part::Other(part_two(input).to_string())).unwrap();
+}
+
+fn part_one(_input: &str) -> usize {
+    99999
+}
+
+// fn part_two(input: &str) -> usize {
+//     99999
+// }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const EXAMPLE_1: &str = r#"47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47"#;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(r"143", part_one(EXAMPLE_1).to_string());
+    }
+
+    // #[test]
+    // fn test_real_input() {
+    //     crate::with_input(2024, 5, do_solve).unwrap();
+    // }
+}

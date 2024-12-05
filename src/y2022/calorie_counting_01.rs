294df9bd8d974d23262e9773f3d1b1c9222cc1ef
Barney Boisvert <bboisvert@gmail.com>
2024-12-04 19:59:23 -0800

    both parts, the second three ways

--- src/y2022/calorie_counting_01.rs
+++ src/y2022/calorie_counting_01.rs
@@ -1,18 +1,77 @@
 use crate::Part;
+use std::cmp::Reverse;
+use std::collections::BinaryHeap;
 use std::sync::mpsc::Sender;
 
 pub fn do_solve(input: &str, tx: Sender<Part>) {
-    tx.send(Part::Other(part_one(input).to_string())).unwrap();
-    // tx.send(Part::Other(part_two(input).to_string())).unwrap();
+    let model = parse(input);
+    tx.send(Part::Parse()).unwrap();
+    tx.send(Part::A(part_one(&model).to_string())).unwrap();
+    tx.send(Part::B(part_two_sort(&model).to_string())).unwrap();
+    tx.send(Part::B(part_two_heap(&model).to_string())).unwrap();
+    tx.send(Part::B(part_two_ifs_and_vars(&model).to_string()))
+        .unwrap();
 }
 
-fn part_one(_input: &str) -> usize {
-    99999
+type Model = Vec<usize>;
+
+fn parse(input: &str) -> Model {
+    let mut result = Vec::new();
+    let mut sum = 0;
+    for l in input.lines() {
+        if l == "" {
+            result.push(sum);
+            sum = 0;
+        } else {
+            sum += l.parse::<usize>().unwrap();
+        }
+    }
+    if sum > 0 {
+        result.push(sum);
+    }
+    result
 }
 
-// fn part_two(input: &str) -> usize {
-//     99999
-// }
+fn part_one(model: &Model) -> usize {
+    *model.iter().max().unwrap()
+}
+
+fn part_two_sort(model: &Model) -> usize {
+    let mut model = model.clone();
+    model.sort();
+    model[model.len() - 3..].iter().sum()
+}
+
+fn part_two_heap(model: &Model) -> usize {
+    let mut heap = BinaryHeap::with_capacity(4);
+    for c in model {
+        heap.push(Reverse(c));
+        if heap.len() > 3 {
+            heap.pop();
+        }
+    }
+    heap.iter().map(|r| r.0).sum()
+}
+
+fn part_two_ifs_and_vars(model: &Model) -> usize {
+    let mut a = 0;
+    let mut b = 0;
+    let mut c = 0;
+    for &n in model {
+        if n > b {
+            if n > a {
+                (b, c) = (a, b);
+                a = n;
+            } else {
+                c = b;
+                b = n;
+            }
+        } else if n > c {
+            c = n;
+        }
+    }
+    a + b + c
+}
 
 #[cfg(test)]
 mod test {
@@ -35,12 +94,15 @@ mod test {
 
     #[test]
     fn example_1() {
-        assert_eq!(r"24000", part_one(EXAMPLE_1).to_string());
-        // assert_eq!(r"45000", part_two(EXAMPLE_1).to_string());
+        let model = parse(EXAMPLE_1);
+        assert_eq!(r"24000", part_one(&model).to_string());
+        assert_eq!(r"45000", part_two_sort(&model).to_string());
+        assert_eq!(r"45000", part_two_heap(&model).to_string());
+        assert_eq!(r"45000", part_two_ifs_and_vars(&model).to_string());
     }
 
-    // #[test]
-    // fn test_real_input() {
-    //     crate::with_input(2022, 1, do_solve).unwrap();
-    // }
+    #[test]
+    fn test_real_input() {
+        crate::with_input(2022, 1, do_solve).unwrap();
+    }
 }

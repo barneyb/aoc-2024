e333ca38b8432951bd9ce7f48f2073499dbcf40c
Barney Boisvert <bboisvert@gmail.com>
2024-12-11 08:01:21 -0800

    part one

--- src/y2024/plutonian_pebbles_11.rs
+++ src/y2024/plutonian_pebbles_11.rs
@@ -2,12 +2,47 @@ use crate::Part;
 use std::sync::mpsc::Sender;
 
 pub fn do_solve(input: &str, tx: Sender<Part>) {
-    tx.send(Part::Other(part_one(input).to_string())).unwrap();
+    let stones = parse(input);
+    tx.send(Part::Parse()).unwrap();
+    tx.send(Part::A(part_one(&stones).to_string())).unwrap();
     // tx.send(Part::Other(part_two(input).to_string())).unwrap();
 }
 
-fn part_one(_input: &str) -> usize {
-    99999
+type Stones = Vec<usize>;
+
+fn parse(input: &str) -> Stones {
+    input
+        .trim()
+        .split_ascii_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect()
+}
+
+fn part_one(stones: &Stones) -> usize {
+    let mut stones = blink(stones);
+    for _ in 2..=25 {
+        stones = blink(&stones);
+    }
+    stones.len()
+}
+
+fn blink(stones: &Stones) -> Stones {
+    let mut next = Vec::with_capacity(stones.len() * 3 / 2);
+    for &s in stones {
+        if s == 0 {
+            next.push(1);
+        } else {
+            let len = s.to_string().len();
+            if len % 2 == 0 {
+                let splitter = 10_usize.pow(len as u32 / 2);
+                next.push(s / splitter);
+                next.push(s % splitter);
+            } else {
+                next.push(s * 2024);
+            }
+        }
+    }
+    next
 }
 
 // fn part_two(input: &str) -> usize {
@@ -22,11 +57,12 @@ mod test {
 
     #[test]
     fn example_1() {
-        assert_eq!(r"55312", part_one(EXAMPLE_1).to_string());
+        let stones = parse(EXAMPLE_1);
+        assert_eq!(r"55312", part_one(&stones).to_string());
     }
 
-    // #[test]
-    // fn test_real_input() {
-    //     crate::with_input(2024, 11, do_solve).unwrap();
-    // }
+    #[test]
+    fn test_real_input() {
+        crate::with_input(2024, 11, do_solve).unwrap();
+    }
 }

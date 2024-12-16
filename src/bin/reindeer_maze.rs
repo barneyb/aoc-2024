2be8223f3827553fee268b972a7a2db323edd505
Barney Boisvert <bboisvert@gmail.com>
2024-12-15 21:20:24 -0800

    skeleton for day 16: Reindeer Maze

---
+++ src/bin/reindeer_maze.rs
@@ -0,0 +1,6 @@
+use aoc::y2024::reindeer_maze_16::do_solve;
+use std::io::Error;
+
+fn main() -> Result<(), Error> {
+    aoc::with_input(2024, 16, do_solve)
+}
---
+++ src/y2024/reindeer_maze_16.rs
@@ -0,0 +1,46 @@
+use crate::Part;
+use std::sync::mpsc::Sender;
+
+pub fn do_solve(input: &str, tx: Sender<Part>) {
+    tx.send(Part::Other(part_one(input).to_string())).unwrap();
+    // tx.send(Part::Other(part_two(input).to_string())).unwrap();
+}
+
+fn part_one(_input: &str) -> usize {
+    99999
+}
+
+// fn part_two(input: &str) -> usize {
+//     99999
+// }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const EXAMPLE_1: &str = r#"###############
+#.......#....E#
+#.#.###.#.###.#
+#.....#.#...#.#
+#.###.#####.#.#
+#.#.#.......#.#
+#.#.#####.###.#
+#...........#.#
+###.#.#####.#.#
+#...#.....#.#.#
+#.#.#.###.#.#.#
+#.....#...#.#.#
+#.###.#.#.#.#.#
+#S..#.....#...#
+###############"#;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(r"#################", part_one(EXAMPLE_1).to_string());
+    }
+
+    // #[test]
+    // fn test_real_input() {
+    //     crate::with_input(2024, 16, do_solve).unwrap();
+    // }
+}

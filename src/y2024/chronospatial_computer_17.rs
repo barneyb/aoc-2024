4cdf19e3fc2c04facb62b0db49aae81d97f62e31
Barney Boisvert <bboisvert@gmail.com>
2024-12-16 21:00:43 -0800

    skeleton for day 17: Chronospatial Computer

---
+++ src/y2024/chronospatial_computer_17.rs
@@ -0,0 +1,36 @@
+use crate::Part;
+use std::sync::mpsc::Sender;
+
+pub fn do_solve(input: &str, tx: Sender<Part>) {
+    tx.send(Part::Other(part_one(input).to_string())).unwrap();
+    // tx.send(Part::Other(part_two(input).to_string())).unwrap();
+}
+
+fn part_one(_input: &str) -> usize {
+    99999
+}
+
+// fn part_two(input: &str) -> usize {
+//     99999
+// }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const EXAMPLE_1: &str = r#"Register A: 729
+Register B: 0
+Register C: 0
+
+Program: 0,1,5,4,3,0"#;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(r"4,6,3,5,6,3,5,2,1,0", part_one(EXAMPLE_1).to_string());
+    }
+
+    // #[test]
+    // fn test_real_input() {
+    //     crate::with_input(2024, 17, do_solve).unwrap();
+    // }
+}

d0da199398d847ead3bee3f76fd115f55925ce17
Barney Boisvert <bboisvert@gmail.com>
2024-12-19 06:44:51 -0800

    part one via regex

--- src/y2024/linen_layout_19.rs
+++ src/y2024/linen_layout_19.rs
@@ -1,13 +1,27 @@
 use crate::Part;
+use regex::Regex;
 use std::sync::mpsc::Sender;
 
 pub fn do_solve(input: &str, tx: Sender<Part>) {
-    tx.send(Part::Other(part_one(input).to_string())).unwrap();
+    tx.send(Part::A(part_one(input).to_string())).unwrap();
     // tx.send(Part::Other(part_two(input).to_string())).unwrap();
 }
 
-fn part_one(_input: &str) -> usize {
-    99999
+fn part_one(input: &str) -> usize {
+    let mut lines = input.lines();
+    let towels: Vec<_> = lines.next().unwrap().split(',').map(|s| s.trim()).collect();
+    lines.next();
+    let stacks: Vec<_> = lines.collect();
+    let mut re = "^(".to_string();
+    for (i, t) in towels.iter().enumerate() {
+        if i > 0 {
+            re.push('|')
+        }
+        re.push_str(t);
+    }
+    re.push_str(")+$");
+    let re = Regex::new(&re).unwrap();
+    stacks.iter().filter(|s| re.is_match(s)).count()
 }
 
 // fn part_two(input: &str) -> usize {
@@ -34,8 +48,8 @@ bbrgwb"#;
         assert_eq!(r"6", part_one(EXAMPLE_1).to_string());
     }
 
-    // #[test]
-    // fn test_real_input() {
-    //     crate::with_input(2024, 19, do_solve).unwrap();
-    // }
+    #[test]
+    fn test_real_input() {
+        crate::with_input(2024, 19, do_solve).unwrap();
+    }
 }

0323df5c7a88f1bd0f344d1fffacd7626b184464
Barney Boisvert <bboisvert@gmail.com>
2024-12-18 21:01:20 -0800

    skeleton for day 19: Linen Layout

---
+++ src/bin/linen_layout.rs
@@ -0,0 +1,6 @@
+use aoc::y2024::linen_layout_19::do_solve;
+use std::io::Error;
+
+fn main() -> Result<(), Error> {
+    aoc::with_input(2024, 19, do_solve)
+}
---
+++ src/y2024/linen_layout_19.rs
@@ -0,0 +1,41 @@
+use crate::Part;
+use std::sync::mpsc::Sender;
+
+pub fn do_solve(input: &str, tx: Sender<Part>) {
+    tx.send(Part::Other(part_one(input).to_string())).unwrap();
+    // tx.send(Part::Other(part_two(input).to_string())).unwrap();
+}
+
+fn part_one(_input: &str) -> usize {
+    99999
+}
+
+// fn part_two(input: &str) -> usize {
+//     99999
+// }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const EXAMPLE_1: &str = r#"r, wr, b, g, bwu, rb, gb, br
+
+brwrr
+bggr
+gbbr
+rrbgbr
+ubwu
+bwurrg
+brgr
+bbrgwb"#;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(r"6", part_one(EXAMPLE_1).to_string());
+    }
+
+    // #[test]
+    // fn test_real_input() {
+    //     crate::with_input(2024, 19, do_solve).unwrap();
+    // }
+}

8b95ec6266f043b0a21bf126d6f4684cb3abd393
Barney Boisvert <bboisvert@gmail.com>
2024-12-22 21:01:14 -0800

    skeleton for day 23: LAN Party

---
+++ src/bin/lan_party.rs
@@ -0,0 +1,6 @@
+use aoc::y2024::lan_party_23::do_solve;
+use std::io::Error;
+
+fn main() -> Result<(), Error> {
+    aoc::with_input(2024, 23, do_solve)
+}
---
+++ src/y2024/lan_party_23.rs
@@ -0,0 +1,63 @@
+use crate::Part;
+use std::sync::mpsc::Sender;
+
+pub fn do_solve(input: &str, tx: Sender<Part>) {
+    tx.send(Part::Other(part_one(input).to_string())).unwrap();
+    // tx.send(Part::Other(part_two(input).to_string())).unwrap();
+}
+
+fn part_one(_input: &str) -> usize {
+    99999
+}
+
+// fn part_two(input: &str) -> usize {
+//     99999
+// }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    const EXAMPLE_1: &str = r#"kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn"#;
+
+    #[test]
+    fn example_1() {
+        assert_eq!(r"t", part_one(EXAMPLE_1).to_string());
+    }
+
+    // #[test]
+    // fn test_real_input() {
+    //     crate::with_input(2024, 23, do_solve).unwrap();
+    // }
+}
